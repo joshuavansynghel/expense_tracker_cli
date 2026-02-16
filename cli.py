447257ebcb3eb19213e0141695c68aa4138246e2
joshuavansynghel <120260688+joshuavansynghel@users.noreply.github.com>
2026-02-16 23:09:54 +0100

    Added menu options to view and filter specific expenses

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 from models.expense import Expense
 from utils.validation import validate_date, validate_amount
+from utils.formatter import print_expenses
 
 def main():
 
@@ -8,14 +9,21 @@ def main():
     expenses = []
 
     while True:
-        expenses.append(add_expense())
-        finished_flag = input("Want to add another expense? (Y/N)")
-        if finished_flag == 'N':
-            break
+        print("What would you like to do?")
+        choice = input(
+            "Type 'add' to add an expense\n"
+            "Type 'view' to view allexpenses\n"
+            "Type 'filter' to filter on specific expenses"
+        )
 
-    for expense in expenses:
-        print(expense)
-
+        match choice:
+            case "add":
+                expenses.append(add_expense())
+            case "view":
+                print_expenses(expenses)
+            case "filter":
+                category_choice = input("Choose a category to filter on: ")
+                print_expenses(filter_expenses(expenses, category_choice))
 
 
 def add_expense():
@@ -30,6 +38,16 @@ def add_expense():
     description_text = input("What is the description of the expense?: ")
 
     return Expense(amount, dt, category_text, description_text)
+
+
+def filter_expenses(expenses, category):
+    filtered_expenses = []
+
+    for expense in expenses:
+        if expense.category == category:
+            filtered_expenses.append(expense)
+
+    return filtered_expenses
 
 
 
--- utils/formatter.py
+++ utils/formatter.py
@@ -1,10 +1,19 @@
 def print_expenses(expenses):
-    print("DATE \t AMOUNT \T CATEGORY \t DESCRIPTION")
+    # Check if expenses is not empty
+    if not expenses:
+        print("No expenses have been added yet")
+        return
 
+    # Format header
+    print("DATE \t| AMOUNT \t| CATEGORY \t| DESCRIPTION")
+
+    # Sort on dates descending and category ascending
     sorted_expenses = sorted(
         expenses,
         key=lambda expense: (-expense.date, expense.category)
     )
 
+    # Print expenses
     for expense in sorted_expenses:
-        print(expense)
+        print(f"{expense.date}\t| {expense.amount}\t| \
+              {expense.category}\t| {expense.description}")
