b0a04a17a1c866f218372d6d1c6a8cda0e7c62d7
Joshua <joshua.vansynghel@radius.com>
2026-02-20 08:35:46 +0100

    Created storage handler which handles json storage and coupled it to expense_service

--- services/expense_service.py
+++ services/expense_service.py
@@ -1,15 +1,17 @@
 from models.expense import Expense
+from storage.expense_storage import ExpenseStorage
 from utils.validation import validate_date, validate_amount
 
 class ExpenseService:
-    def __init__(self):
-        self.expenses = []
+    def __init__(self, expense_storage=None):
+        self.expense_storage = expense_storage or ExpenseStorage()
+        self.expenses = self.expense_storage.load_expenses()
 
     def get_expenses(self):
         return self.expenses
 
     def add_expense(self):
-        # Amount input
+        # Ask user for input and validate 
         amount_text = input("What is the amount of the expense in euros?: ")
         amount = validate_amount(amount_text)
 
@@ -19,9 +21,13 @@ class ExpenseService:
         category_text = input("What is the category of the expense?: ")
         description_text = input("What is the description of the expense?: ")
 
+        # Instantiate expense
         expense = Expense(amount, dt, category_text, description_text)
 
+        # Store locally and persistent
         self.expenses.append(expense)
+        self.expense_storage.store_expenses(self.expenses)
+
 
     def filter_expenses(self, category):
         filtered_expenses = []
--- storage/expense_storage.py
+++ storage/expense_storage.py
@@ -1,2 +1,21 @@
-class ExpenseStorage(self):
-    
+import os
+import json
+
+class ExpenseStorage:
+
+    def __init__(self, file_path="../data"):
+        self.file_path = file_path
+
+    def store_expenses(self, expenses):
+        file_location = os.path.join(self.file_path, "expenses.json")
+
+        with open(file_location, "w") as f:
+            json.dump(expenses, f)
+
+    def load_expenses(self):
+        file_location = os.path.join(self.file_path, "expenses.json")
+
+        with open(file_location, "r") as f:
+            expenses = json.load(f)
+
+        return expenses
