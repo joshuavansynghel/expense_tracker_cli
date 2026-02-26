c2bf5dedd97f0a6f554be8d6d1baba74d503374d
Joshua <joshua.vansynghel@radius.com>
2026-02-26 08:32:17 +0100

    add tests for expense service and fix typo on storage tests

--- tests/test_expense_service.py
+++ tests/test_expense_service.py
@@ -1,7 +1,6 @@
 import pytest 
 
 from expense_tracker.services.expense_service import ExpenseService
-from expense_tracker.models.expense import Expense
 from fake_storage import FakeStorage
 
 @pytest.fixture
@@ -12,7 +11,37 @@ def test_get_expenses_initially_empty(expense_service):
     assert expense_service.get_expenses() == []
 
 def test_add_expense(expense_service):
+    assert expense_service.get_expenses() == []
+
     expense_service.add_expense(75, '2025-04-22', 'Entertainment', 'Restaurant')
 
     assert len(expense_service.get_expenses()) == 1
 
+def test_add_multiple_expenses(expense_service):
+    assert expense_service.get_expenses() == []
+
+    expense_service.add_expense(75, '2025-04-22', 'Entertainment', 'Restaurant')
+    expense_service.add_expense(150, '2026-01-15', 'Groceries', 'Food')
+    expense_service.add_expense(40, '2025-08-31', 'Insurance', 'Car insurance')
+
+    assert len(expense_service.get_expenses()) == 3
+
+def test_filter_expenses(expense_service):
+    assert expense_service.get_expenses() == []
+
+    expense_service.add_expense(75, '2025-04-22', 'Entertainment', 'Restaurant')
+    expense_service.add_expense(150, '2026-01-15', 'Entertainment', 'Cinema')
+    expense_service.add_expense(40, '2025-08-31', 'Insurance', 'Car insurance')
+
+    assert len(expense_service.filter_expenses('Entertainment')) == 2
+    assert len(expense_service.filter_expenses('Insurance')) == 1
+    assert len(expense_service.filter_expenses('Groceries')) == 0
+
+def test_calculate_summary(expense_service):
+    assert expense_service.calculate_summary(expense_service.get_expenses()) == 0
+
+    expense_service.add_expense(75, '2025-04-22', 'Entertainment', 'Restaurant')
+    expense_service.add_expense(150, '2026-01-15', 'Entertainment', 'Cinema')
+    expense_service.add_expense(40, '2025-08-31', 'Insurance', 'Car insurance')
+
+    assert expense_service.calculate_summary(expense_service.get_expenses()) == 265
---
+++ tests/test_expense_storage.py
@@ -0,0 +1,6 @@
+import pytest
+
+from expense_tracker.storage.expense_storage import ExpenseStorage
+
+
+
